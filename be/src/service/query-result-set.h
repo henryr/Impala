ee62bc7a2db04f22c9d22de2a7c78b595c4d4d8e
Henry Robinson <henry@cloudera.com>
2016-10-15 16:47:24 -0700

    query result set
    Change-Id: I6ab883b62d3ec7012240edf8d56889349e7c0e32

--- be/src/service/query-result-set.h
+++ be/src/service/query-result-set.h
@@ -20,7 +20,9 @@
 
 #include "common/status.h"
 #include "gen-cpp/Data_types.h"
+#include "gen-cpp/Results_types.h"
 
+#include <boost/scoped_ptr.hpp>
 #include <vector>
 
 namespace impala {
@@ -58,6 +60,127 @@ class QueryResultSet {
 
   /// Returns the size of this result set in number of rows.
   virtual size_t size() = 0;
+};
+
+/// Ascii result set for Beeswax.
+/// Beeswax returns rows in ascii, using "\t" as column delimiter.
+class AsciiQueryResultSet : public QueryResultSet {
+ public:
+  // Rows are added into rowset.
+  AsciiQueryResultSet(const TResultSetMetadata& metadata, vector<string>* rowset)
+    : metadata_(metadata), result_set_(rowset), owned_result_set_(NULL) {}
+
+  // Rows are added into a new rowset that is owned by this result set.
+  AsciiQueryResultSet(const TResultSetMetadata& metadata)
+    : metadata_(metadata),
+      result_set_(new vector<string>()),
+      owned_result_set_(result_set_) {}
+
+  virtual ~AsciiQueryResultSet() {}
+
+  // Convert expr values (col_values) to ASCII using "\t" as column delimiter and store
+  // it in this result set.
+  // TODO: Handle complex types.
+  virtual Status AddOneRow(
+      const std::vector<void*>& col_values, const std::vector<int>& scales);
+
+  // Convert TResultRow to ASCII using "\t" as column delimiter and store it in this
+  // result set.
+  virtual Status AddOneRow(const TResultRow& row);
+
+  virtual int AddRows(const QueryResultSet* other, int start_idx, int num_rows);
+
+  virtual int64_t ByteSize(int start_idx, int num_rows);
+
+  virtual size_t size() { return result_set_->size(); }
+
+ private:
+  // Metadata of the result set
+  const TResultSetMetadata& metadata_;
+
+  // Points to the result set to be filled. The result set this points to may be owned by
+  // this object, in which case owned_result_set_ is set.
+  std::vector<string>* result_set_;
+
+  // Set to result_set_ if result_set_ is owned.
+  boost::scoped_ptr<std::vector<string>> owned_result_set_;
+};
+
+// Result set container for Hive protocol versions >= V6, where results are returned in
+// column-orientation.
+class HS2ColumnarResultSet : public QueryResultSet {
+ public:
+  HS2ColumnarResultSet(const TResultSetMetadata& metadata, TRowSet* rowset = NULL)
+    : metadata_(metadata), result_set_(rowset), num_rows_(0) {
+    if (rowset == NULL) {
+      owned_result_set_.reset(new TRowSet());
+      result_set_ = owned_result_set_.get();
+    }
+    InitColumns();
+  }
+
+  virtual ~HS2ColumnarResultSet() {}
+
+  // Add a row of expr values
+  virtual Status AddOneRow(const vector<void*>& col_values, const vector<int>& scales);
+  // Add a row from a TResultRow
+  virtual Status AddOneRow(const TResultRow& row);
+  // Copy all columns starting at 'start_idx' and proceeding for a maximum of 'num_rows'
+  // from 'other' into this result set
+  virtual int AddRows(const QueryResultSet* other, int start_idx, int num_rows);
+
+  virtual int64_t ByteSize(int start_idx, int num_rows);
+
+  virtual size_t size() { return num_rows_; }
+
+ private:
+  // Metadata of the result set
+  const TResultSetMetadata& metadata_;
+
+  // Points to the TRowSet to be filled. The row set this points to may be owned by
+  // this object, in which case owned_result_set_ is set.
+  TRowSet* result_set_;
+
+  // Set to result_set_ if result_set_ is owned.
+  scoped_ptr<TRowSet> owned_result_set_;
+
+  int64_t num_rows_;
+
+  void InitColumns();
+};
+
+// TRow result set for HiveServer2
+class HS2RowOrientedResultSet : public QueryResultSet {
+ public:
+  // Rows are added into rowset.
+  HS2RowOrientedResultSet(const TResultSetMetadata& metadata, TRowSet* rowset = NULL)
+    : metadata_(metadata), result_set_(rowset) {
+    if (rowset == NULL) {
+      owned_result_set_.reset(new TRowSet());
+      result_set_ = owned_result_set_.get();
+    }
+  }
+
+  virtual ~HS2RowOrientedResultSet() {}
+
+  // Convert expr value to HS2 TRow and store it in TRowSet.
+  virtual Status AddOneRow(const vector<void*>& col_values, const vector<int>& scales);
+  // Convert TResultRow to HS2 TRow and store it in TRowSet.
+  virtual Status AddOneRow(const TResultRow& row);
+  virtual int AddRows(const QueryResultSet* other, int start_idx, int num_rows);
+  virtual int64_t ByteSize(int start_idx, int num_rows);
+  virtual size_t size() { return result_set_->rows.size(); }
+
+ private:
+  // Metadata of the result set
+  const TResultSetMetadata& metadata_;
+
+  // Points to the TRowSet to be filled. The row set this points to may be owned by
+  // this object, in which case owned_result_set_ is set.
+  TRowSet* result_set_;
+
+  // Set to result_set_ if result_set_ is owned.
+  scoped_ptr<TRowSet> owned_result_set_;
 };
 }
 
