42643c7bdbe1a2247427ccb828ac5621ef725508
Henry Robinson <henry@cloudera.com>
2016-11-08 12:06:48 -0800

    IMPALA-4669: [KUTIL] Minor changes to make kutil compile
    Change-Id: I8da798ee55506d6f969416b17c191eb03cb215f5

--- be/src/kudu/util/logging.cc
+++ be/src/kudu/util/logging.cc
@@ -41,9 +41,10 @@
 #include "kudu/util/flag_tags.h"
 #include "kudu/util/status.h"
 
-DEFINE_string(log_filename, "",
-    "Prefix of log filename - "
-    "full path is <log_dir>/<log_filename>.[INFO|WARN|ERROR|FATAL]");
+// TODO: HNR -> sort this out.
+DECLARE_string(log_filename); // , "",
+// "Prefix of log filename - "
+// "full path is <log_dir>/<log_filename>.[INFO|WARN|ERROR|FATAL]");
 TAG_FLAG(log_filename, stable);
 
 DEFINE_bool(log_async, true,
@@ -56,9 +57,10 @@ DEFINE_int32(log_async_buffer_bytes_per_level, 2 * 1024 * 1024,
              "level. Only relevant when --log_async is enabled.");
 TAG_FLAG(log_async_buffer_bytes_per_level, hidden);
 
-DEFINE_int32(max_log_files, 10,
-    "Maximum number of log files to retain per severity level. The most recent "
-    "log files are retained. If set to 0, all log files are retained.");
+DECLARE_int32(max_log_files);
+// DEFINE_int32(max_log_files, 10,
+//     "Maximum number of log files to retain per severity level. The most recent "
+//     "log files are retained. If set to 0, all log files are retained.");
 TAG_FLAG(max_log_files, runtime);
 TAG_FLAG(max_log_files, experimental);
 
