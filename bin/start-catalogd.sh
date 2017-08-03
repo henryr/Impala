740faac002bc0bf56a266246dbb44f2b8f078d1a
Henry Robinson <henry@cloudera.com>
2017-08-02 22:43:22 -0700

    IMPALA-5758: Enable LSAN
    Change-Id: I1f6375308c65f3e66313dae9853baf0bdbc82a68

--- bin/start-catalogd.sh
+++ bin/start-catalogd.sh
@@ -70,7 +70,8 @@ if ${CLUSTER_DIR}/admin is_kerberized; then
 fi
 
 . ${IMPALA_HOME}/bin/set-classpath.sh
-export ASAN_OPTIONS="handle_segv=0 detect_leaks=0 allocator_may_return_null=1"
+export ASAN_OPTIONS="handle_segv=0:detect_leaks=1:allocator_may_return_null=1"
+export LSAN_OPTIONS="suppressions=${IMPALA_HOME}/lsan_suppressions.txt"
 export UBSAN_OPTIONS="print_stacktrace=1"
 UBSAN_OPTIONS="${UBSAN_OPTIONS} suppressions=${IMPALA_HOME}/bin/ubsan-suppressions.txt"
 export PATH="${IMPALA_TOOLCHAIN}/llvm-${IMPALA_LLVM_VERSION}/bin:${PATH}"
